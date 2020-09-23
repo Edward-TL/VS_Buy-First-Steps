0412ff09ac0c50f82592ac395f9b1305c3779988
Edward-TL <edward.toledo.lopez@hotmail.com>
2020-09-23 17:32:04 -0500

    assertEquals alarm solved. Improved the get_cheapest as a dictionary test. Because of the possibility that the cheapest's link could be really long (actually it happened), the test change to a IsNotNone test for every value in the dictionary.

--- Scraper/Mercado_Libre/tests.py
+++ Scraper/Mercado_Libre/tests.py
@@ -2,7 +2,8 @@ import unittest
 
 from data import Mercado_Libre
 from scrape_funcs import extract_soup, search_boxes, get_brute_info
-from data_filters import get_names, get_images, get_products_urls, get_price
+from page_getters import get_names, get_images, get_products_urls, get_price
+from cheapest_funcs import cheapest, get_cheapest
 
 
 class Test_Mercado_Libre_Properties_And_Functions(unittest.TestCase):
@@ -51,13 +52,28 @@ class Test_Mercado_Libre_Properties_And_Functions(unittest.TestCase):
 
         getters = {'ml_names' : len(get_names(ml_boxes, Mercado_Libre.name_and_images)),
         'ml_images' : len(get_images(ml_boxes, Mercado_Libre.name_and_images)),
-        # 'ml_urls' : len(get_products_urls(ml_boxes, Mercado_Libre.product_urls)),
-        # 'ml_price' : len(get_price(country, ml_boxes, Mercado_Libre.price)),
-        # 'ml_ids' : len(ml_products_id(ml_boxes)),
-        # 'ml_reviews' : len(get_reviews(country, ml_boxes, Mercado_Libre.reviews)),
-        # 'ml_stars' : len(get_stars(country, ml_boxes, Mercado_Libre.stars)),
+        'ml_urls' : len(get_products_urls(ml_boxes, Mercado_Libre.product_urls)),
+        'ml_price' : len(get_price(country, ml_boxes, Mercado_Libre.price)),
         }
         
-        for value in range(len(getters)):
-            self.assertEquals(len(ml_boxes), getters[value])
+        for value in getters:
+            self.assertEqual(len(ml_boxes), getters[value])
+    
+    
+
+    def test_cheapest_gets_info(self):
+        user_request = 'audifonos inalambricos'
+        country = 'mx'
+
+        ml_url = Mercado_Libre.adapt_url(Mercado_Libre, country, user_request)
+        ml_soup = extract_soup(ml_url, 1, just_soup=True)
+        ml_boxes = search_boxes(ml_soup, Mercado_Libre.boxes)
+        meli_prices= get_price(country, ml_boxes, Mercado_Libre.price)
+
+        meli_cheapest_idx, meli_cheapest_price = cheapest(meli_prices, position_and_price=True)
+        cheapest_ml_product_1 = get_cheapest(meli_cheapest_idx, ml_boxes, meli_cheapest_price, country, Mercado_Libre)
+
+        for value in cheapest_ml_product_1:
+            self.assertIsNotNone(cheapest_ml_product_1[value])
+
 unittest.main()
