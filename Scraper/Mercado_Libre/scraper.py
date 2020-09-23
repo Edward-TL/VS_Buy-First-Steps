f01d8e44433a83d061b429af32f42e5f81bb32bf
Edward-TL <edward.toledo.lopez@hotmail.com>
2020-09-23 12:56:38 -0500

    Retruns cheapest info by two ways: just with the price array and page_boxes array, or with a dict with all the info extracted

--- Scraper/Mercado_Libre/scraper.py
+++ Scraper/Mercado_Libre/scraper.py
@@ -14,30 +14,27 @@ ml_soup = extract_soup(ml_url, 1, just_soup=True)
 
 # #HTML divided by products, and stored as elements of an array
 ml_boxes = search_boxes(ml_soup, Mercado_Libre.boxes)
-# print('boxes:', len(ml_boxes))
-# ml_products = {}
-
-# ml_products['names'] = get_names(ml_boxes, Mercado_Libre.name_and_images)
-
-# #Mercado_Libre's images source (link)
-# ml_products['images'] = get_images(ml_boxes, Mercado_Libre.name_and_images)
-
-# ml_products['urls'] = get_products_urls(ml_boxes, Mercado_Libre.product_urls)
-
-# ml_products['prices'] = get_price(country, ml_boxes, Mercado_Libre.price)
-
-# cheapest = cheapest(ml_products['prices'])
-# cheapest_ml_product = get_cheapest(cheapest, ml_products)
-
-# print(f'Test ONE:')
-# for key in cheapest_ml_product:
-#     print(key, ':', cheapest_ml_product[key])
-
-print(f'\n\nTest TWO:')
+print(f'Test ONE:')
 meli_prices= get_price(country, ml_boxes, Mercado_Libre.price)
 
-meli_cheapest_idx, meli_cheapest_price = cheapest(meli_prices)
+meli_cheapest_idx, meli_cheapest_price = cheapest(meli_prices, position_and_price=True)
 cheapest_ml_product_2 = get_cheapest(meli_cheapest_idx, ml_boxes, meli_cheapest_price, country, Mercado_Libre)
 
 for key in cheapest_ml_product_2:
     print(key, ':', cheapest_ml_product_2[key])
+
+# print('boxes:', len(ml_boxes))
+ml_products = {}
+
+ml_products['names'] = get_names(ml_boxes, Mercado_Libre.name_and_images)
+#Mercado_Libre's images source (link)
+ml_products['images'] = get_images(ml_boxes, Mercado_Libre.name_and_images)
+ml_products['urls'] = get_products_urls(ml_boxes, Mercado_Libre.product_urls)
+ml_products['prices'] = get_price(country, ml_boxes, Mercado_Libre.price)
+
+cheapest_idx = cheapest(ml_products['prices'])
+cheapest_ml_product = get_cheapest(cheapest_idx, ml_products)
+
+print(f'\n\nTest TWO:')
+for key in cheapest_ml_product:
+    print(key, ':', cheapest_ml_product[key])
