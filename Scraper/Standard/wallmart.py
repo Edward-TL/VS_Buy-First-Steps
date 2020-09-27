9ecafd740d51c7faa767ae349d60985a73a9dabc
Edward-TL <edward.toledo.lopez@hotmail.com>
2020-09-27 16:22:01 -0500

    [TEST] Fail to add
    Even using selenium, Wallmart seems to be strict with the consult. So,
    in order to not break any law or page's rule, the option of add wallmart
    is dissmissed.
    
    In addition that is xtremely low (around 6 seg.) for a web consult

--- Scraper/Standard/wallmart.py
+++ Scraper/Standard/wallmart.py
@@ -8,10 +8,13 @@ from selenium.webdriver.common.keys import Keys
 #VS_Buy Package
 from data import headers, Wallmart
 from scrape_funcs import extract_soup, search_boxes
+from page_getters import get_names, get_images, get_products_urls, get_price
+from cheapest_funcs import cheapest, get_cheapest
 
 
 if __name__ == '__main__':
     wallmart_url = 'https://www.walmart.com.mx/productos?Ntt=xbox%20one%20consola'
+    country = 'mx'
     # driver = webdriver.PhantomJS(executable_path = "/usr/local/Cellar/phantomjs/2.1.1/bin/phantomjs")
     driver = webdriver.Chrome("chromedriver.exe")
     
@@ -19,16 +22,37 @@ if __name__ == '__main__':
     driver.get(wallmart_url)
 
     selenium_html = driver.page_source
-    selenium_soup = BeautifulSoup(selenium_html)
-    
+    selenium_soup = BeautifulSoup(selenium_html, features='lxml')
+    print(selenium_soup.prettify())
+    wallmart_boxes = search_boxes(selenium_soup, Wallmart.boxes)
+    for box in wallmart_boxes:
+        print(box.prettify())
 
-    page_boxes = search_boxes(selenium_soup, Wallmart.boxes)
     # From this part, could get better AFTER the 4 scrapers are made
     #From the Boxes, obtain the prices
-    price_boxes = get_price(country, page_boxes, Page, test_len=True)
-    #Obtain the cheapest from prices and then, you obtain the cheapest product as a dictionary
-    cheapest_idx, cheapest_price = cheapest(price_boxes, position_and_price=True)
-    cheapest_product_dictionary = get_cheapest(cheapest_idx, page_boxes, cheapest_price, country, Page)
+    wallmart_products={}
 
+    wallmart_products['name'] = get_names(wallmart_boxes, Wallmart, test_all=True)
 
-    driver.close()
+    '''Wallmart's images source (link)'''
+    # wallmart_products['image'] = get_images(wallmart_boxes, Wallmart, test_all=True)
+
+    # wallmart_products['url'] = get_products_urls(wallmart_boxes, Wallmart, test_all=True)
+
+    '''Just Wallmart's products id. Is used as a url generator:
+    wallmart's url + domain + "/dp/" + product_id'''
+    # wallmart_products['id']= wallmart_products_id(wallmart_boxes)
+
+    # wallmart_products['price'] = get_price(country, wallmart_boxes, Wallmart.price, test_all=True)
+    # print(len(wallmart_reviews))
+    # for key in wallmart_products:
+    #     print(key, ':', wallmart_products[key])
+
+    # cheapest = cheapest(wallmart_products['price'])
+    # cheapest_wallmart_product = get_cheapest(cheapest, wallmart_products)
+    # for key in cheapest_wallmart_product:
+    #     print(key, ':', cheapest_wallmart_product[key])
+    # for highlight in wallmart_highlightners:
+    #     print(highlight)
+    if len(wallmart_boxes) > 0:
+        driver.close()
