c533dde0e3e495e9f452fe2cf3f33c36c2e84768
Edward <64107083+Edward-TL@users.noreply.github.com>
2020-10-04 20:32:36 -0500

    [FEAT] final version
    All needed comments added.
    
    Correction on Pages index

--- Scraper/Standard/data.py
+++ Scraper/Standard/data.py
@@ -3,6 +3,8 @@ from dataclasses import dataclass
 @dataclass
 class Page:
     name:str
+    '''Page index given by the backend'''
+    index: int
     '''Page URl with name of the format replacers'''
     url : str      
     page_test : str
@@ -30,29 +32,38 @@ class Page:
     money_dict : dict
     
     def adapt_url(self, Page, user_request, country_domain):
-        check = str(type(Page.url_replacers))
+        # Common to forget the dot in the domain
         if country_domain[0] != ".":
             country_domain = '.' + country_domain
         
-        
+        #When is one, the tuple turns into a str (in this case)
+        check = str(type(Page.url_replacers))
         if check != "<class 'str'>":
             adapted_url = Page.url.replace(Page.url_replacers[0], country_domain)
             for r in range(1, len(Page.url_replacers)):
                 user_request_adapted = user_request.replace(' ', Page.space_replacer[r-1])
                 adapted_url = adapted_url.replace(Page.url_replacers[r], user_request_adapted)
+
+        # But, because is not that usual, is left here, as not the principal case
         else:
             user_request_adapted = user_request.replace(' ', Page.space_replacer[0])
             adapted_url = Page.url.replace(Page.url_replacers, user_request_adapted)
         
         return adapted_url
 
+# Here starts the creation of the pages
+# Each one starts with the money dict creation
+
+# --------------------------------------------
+#                A M A Z O N
+# --------------------------------------------
 amazon_money_dict = {'mx' : {'coin' : '$',
                       'thousands': ',',
                       'decimal': '.',
                       'two_prices_sep' : ' - '}
-}
-
+                    }
 Amazon = Page(name = 'Amazon',
+    index = 1,
     url='https://www.amazon.com{country}/s?k={user_request}',
     page_test='https://www.amazon.com.mx/s?k=audifonos+inalambricos',
     url_replacers=('{country}', '{user_request}'),
@@ -71,13 +82,16 @@ Amazon = Page(name = 'Amazon',
     #product_id('data-asin')
     )
 
+# --------------------------------------------
+#                    E B A Y
+# --------------------------------------------
 ebay_money_dict = {'mx' : {'coin' : 'MXN $',
                       'thousands': ' ',
                       'decimal': '.',
                       'two_prices_sep' : ' a '}
             }
-
 Ebay = Page(name='Ebay',
+    index = 2,
     url='https://www.ebay.com/sch/i.html?_nkw={user_request}',
     page_test='https://www.ebay.com/sch/i.html?_nkw=audifonos+inalambricos',
     url_replacers=('{user_request}'),
@@ -94,13 +108,18 @@ Ebay = Page(name='Ebay',
     price=('span', 'class', 's-item__price'),
     money_dict=ebay_money_dict,)
 
+# --------------------------------------------
+#         M E R C A D O   L I B R E
+# --------------------------------------------
+# Fun fact: In the API, they called everything as meli
+# short of MErcado LIbre. 
 meli_money_dict = {'mx' : {'coin' : '$',
                       'thousands': ',',
                       'decimal': '.',
                       'two_prices_sep' : ' - '}
             }
-            
 Mercado_Libre = Page(name='Mercado Libre',
+    index = 3,
     url='https://listado.mercadolibre.com{country}/{user_request_1}#D[A:{user_request_2}]',
     page_test='https://listado.mercadolibre.com.mx/audifonos-inalambricos#D[A:audifonos%20inalambricos]',
     url_replacers=('{country}', '{user_request_1}', '{user_request_2}'),
@@ -117,37 +136,16 @@ Mercado_Libre = Page(name='Mercado Libre',
     price=('span', 'class', 'price-tag ui-search-price__part'),
     money_dict=meli_money_dict,)
 
-# wallmart_money_dict = {'mx' : {'coin' : '$',
-#                       'thousands': ',',
-#                       'decimal': '.',
-#                       'two_prices_sep' : ' - '}
-# }
-
-# Wallmart = Page(name = 'Wallmart',
-#     url='https://www.walmart.com{country}/productos?Ntt={user_request}',
-#     url_replacers=('{country}', '{user_request}'),
-#     space_replacer=['%20'],
-#     boxes=('div', 'class', 'grid_product__30OQa'),
-#     highlights=None,
-#     product_urls=('a', 'class', 'nav-link_navLink__2oJ29 product_image__LcsAj'),
-#     url_get='href',
-#     name_and_images=('span', 'class', 'lazyload-wrapper'),
-#     names_get='alt',
-#     images_get='src',
-#     reviews=None,
-#     stars=None,
-#     price=('p', 'data-automation-id', 'sale-price'),
-#     money_dict=wallmart_money_dict,
-#     #product_id('data-asin')
-#     )
-
+# --------------------------------------------
+#              B E S T   B U Y
+# --------------------------------------------
 best_buy_money_dict = {'mx' : {'coin' : '$',
                       'thousands': ',',
                       'decimal': '.',
                       'two_prices_sep' : ' - '}
 }
-
 Best_Buy = Page(name = 'Best Buy',
+    index = 4,
     url='https://www.bestbuy.com{country}/c/buscar-best-buy/buscar?query={user_request}',
     page_test='https://www.bestbuy.com.mx/c/buscar-best-buy/buscar?query=audifonos+inalambricos',
     url_replacers=('{country}', '{user_request}'),
@@ -168,6 +166,12 @@ Best_Buy = Page(name = 'Best Buy',
 
 Pages = [Amazon, Mercado_Libre, Ebay, Best_Buy]
 
+
+# --------------------------------------------
+#             END OF PAGES CLASS
+# --------------------------------------------
+
+# THIS IS THE INFO FOR THE SCRAPER HEADER
 class headers:
     wallmart = {'accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
                 'accept-encoding':'gzip, deflate, br',
@@ -200,15 +204,3 @@ class headers:
     common = (h0, h2, h2)
     h3 = wallmart
     all_saved = (h0, h2, h2, wallmart)
-
-class Products:
-    def __init__(self, names, images_links, products_links, prices):
-        self.names = names
-        self.images = images_links
-        self.products_links = products_links
-        self.prices = prices
-
-
-
-if __name__ == '__main__':
-    print(Amazon)
