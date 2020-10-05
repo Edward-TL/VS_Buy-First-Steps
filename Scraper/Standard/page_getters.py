4fada357bcd43a0f81a8209a1b0466c2f3e9857c
Edward <64107083+Edward-TL@users.noreply.github.com>
2020-10-04 20:45:48 -0500

    [STYLE] Comments
    Comments add and final issues solved

--- Scraper/Standard/page_getters.py
+++ Scraper/Standard/page_getters.py
@@ -1,15 +1,13 @@
 from scrape_funcs import search_boxes
+from data import Page
 import re
 
-def get_names(boxes_array, Page, test_all=False, test_len=False, position=None):
+def get_names(boxes_array, Page, position=None):
     names = [None]*len(boxes_array)
     name = Page.names_get
 
-    if test_all == True:
-            print(f'Boxes array len: {len(boxes_array)}')
-
-    '''If you know want to know some info of an specific product by its position on the page.
-    Like you know the position of the cheapest'''
+    # If you know want to know some info of an specific product by its position on the page.
+    # Like you know the position of the cheapest
     if position != None:
         searcher = search_boxes(boxes_array[position], Page.name_and_images)
         if searcher:
@@ -20,36 +18,23 @@ def get_names(boxes_array, Page, test_all=False, test_len=False, position=None):
             else:
                 names = searcher[0].img.get(name)
     else:
-        '''For Testing the functions and Xpaths'''
+    # Obtain all the info
         b=0
-        if test_all == True:
-            print('For Loop')
         for box in boxes_array:
             #Remember that boxes are arrays
             searcher = search_boxes(box, Page.name_and_images)
             if searcher:
-                try:
-                    if Page.name == 'Best Buy':
-                        image_name = searcher[0].img.get(name)
-                        name_split = image_name.split(' - ')
-                        names[b] = name_split[1]
-                    else:
-                        names[b] = searcher[0].img.get(name)
-                except:
-                    name_message_error = f'''Value info:
-                    box: {box}
-                    Searcher: {searcher}
-                    Searcher[0].img = {searcher[0].img}'''
-
-                    raise ValueError(name_message_error)
+                if Page.name == 'Best Buy':
+                    image_name = searcher[0].img.get(name)
+                    name_split = image_name.split(' -')
+                    if len(name_split) > 1:
+                        names[b] = name_split[1].lstrip()
+                    if len(name_split) == 1:
+                        names[b] = name_split[0].lstrip()
+                else:
+                    names[b] = searcher[0].img.get(name)
                 
             b +=1
-    if test_all == True:
-        print(names)
-    
-    elif test_len == True:
-        print(len(names))
-
     return names
 
 
@@ -57,15 +42,15 @@ def get_images(boxes_array, Page, test_all=False, test_len=False, position=None)
     images = [None]*len(boxes_array)
     image = Page.images_get
     
-    '''If you know want to know some info of an specific product by its position on the page.
-    Like you know the position of the cheapest'''
+    # If you know want to know some info of an specific product by its position on the page.
+    # Like you know the position of the cheapest
     if position:
         searcher = search_boxes(boxes_array[position], Page.name_and_images)
 
         if searcher:
             images = searcher[0].img.get(image)
     else:
-        '''For Testing the functions and Xpaths'''
+    # Obtain all the info
         b=0
         for box in boxes_array:
             #Remember that boxes are arrays
@@ -74,21 +59,14 @@ def get_images(boxes_array, Page, test_all=False, test_len=False, position=None)
                 images[b] = searcher[0].img.get(image)
             b +=1
 
-    if test_all == True:
-        print(images)
-    
-    elif test_len == True:
-        print('images:', len(images))
-
     return images
 
 def get_products_urls(boxes_array, Page, test_all=False, test_len=False, position=None):
     urls = [None]*len(boxes_array)
     url = Page.url_get
-    if test_all == True:
-        print('Urls:')
-    '''If you know want to know some info of an specific product by its position on the page.
-    Like you know the position of the cheapest'''
+    
+    # If you know want to know some info of an specific product by its position on the page.
+    # Like you know the position of the cheapest
     if position:
         searcher = search_boxes(boxes_array[position], Page.product_urls)
         if searcher:
@@ -112,16 +90,12 @@ def get_products_urls(boxes_array, Page, test_all=False, test_len=False, positio
 
                 raise ValueError(error_message)
     else:
-        '''For Testing the functions and Xpaths'''
+        # Obtain all the info
         b=0
         for box in boxes_array:
-            if test_all == True:
-                print('For loop:')
             #Remember that boxes are arrays
             searcher = search_boxes(box, Page.product_urls)
             if searcher:
-                if test_all == True:
-                    print(searcher[0].a)
                 if Page.name == 'Amazon':
                     source_url = searcher[0].get(url)
                     urls[b] = 'https://www.amazon.com.mx' + source_url
@@ -130,11 +104,6 @@ def get_products_urls(boxes_array, Page, test_all=False, test_len=False, positio
                     urls[b] = searcher[0].a.get(url)
             b +=1
 
-    if test_all == True:
-        print(urls)
-    
-    elif test_len == True:
-        print('urls:', len(urls))
     return urls
 
 def get_price(country, boxes_array, Page, test_all=False, test_len=False, position=None):
@@ -155,7 +124,10 @@ def get_price(country, boxes_array, Page, test_all=False, test_len=False, positi
                 price_string = price_string[0].replace(coin_symbol,'').replace(k_sep,'').replace(d_sep,'.')
                 #Special case
                 price_string = re.findall(r'(\d+\.\d+)', price_string)
-                price = float(price_string[0])
+                if Page.name != 'Ebay':
+                    price = round(float(price_string[0])/22, 2) 
+                else:
+                    price = float(price_string[0])
             except:
                 error_message = f'''String index out of range. 
                 Money dictionary: {Page.money_dict}
@@ -169,41 +141,33 @@ def get_price(country, boxes_array, Page, test_all=False, test_len=False, positi
         for box in boxes_array:
             #Remember that boxes are arrays
             searcher = search_boxes(box, Page.price)
-            if test_all == True:
-                print(searcher)
             if searcher:
                 if country == 'mx':
-                    try:
-                        price_string = searcher[0].get_text().split(tps)
-                        if test_all == True:
-                            print(price_string)
-                        price_string_bfre = price_string[0].replace(coin_symbol,'').replace(k_sep,'').replace(d_sep,'.')
-                        if test_all == True:
-                            print(price_string) 
-                        #Ebays Special case
-                        price_string_bfre = price_string_bfre.replace('\xa0','')
-                        #Just in case
-                        price_string_check = re.findall(rf"(\d+\.?\d+)", price_string_bfre)
-                        price[b] = float(price_string_check[0])                        
-
-                    except:
-                        error_message = f'''Info about the Value. 
-                        Money dictionary: {Page.money_dict}
-                        Original String: {searcher[0].get_text()}
-                        Before RegEx: {price_string_bfre}
-                        Type Before RegEx: {type(price_string)}
-                        Price string: {price_string}
-                        Box #{b}'''
-                        raise ValueError(error_message)                        
+                    # try:
+                    price_string = searcher[0].get_text().split(tps)
+                    price_string_bfre = price_string[0].replace(coin_symbol,'').replace(k_sep,'').replace(d_sep,'.')
+                    #Ebays Special case
+                    price_string_bfre = price_string_bfre.replace(u'\xa0',u'')
+                    #Just in case
+                    price_string_check = re.findall(rf"(\d+\.?\d+)", price_string_bfre)
+                    #Sometimes it needs to be done again
+                    #           
+                    if Page.name != 'Ebay':
+                        price[b] = round(float(price_string_check[0])/22, 2) 
+                    else:
+                        if str(type(price_string_check)) == "<class 'list'>" and len(price_string_check) > 0:
+                            string_search = price_string_check[0]
+                        else:
+                            string_search = price_string_check
+                        if str(type(string_search)) == "<class 'str'>":
+                            price[b] = float(string_search)
             b +=1
 
-    if test_all == True:
-        print('prices:', len(price), price)
-    
-    elif test_len == True:
-        print('prices:', len(price))
-
     return price
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Info that can be obtained for some pages, but are not a standard for all
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 def get_stars(boxes_array, Page, country='mx', test_all=False, test_len=False, position=None):
     stars = [None]*len(boxes_array)
@@ -213,7 +177,6 @@ def get_stars(boxes_array, Page, country='mx', test_all=False, test_len=False, p
     for box in boxes_array:
         #Remember that boxes are arrays
         searcher = search_boxes(box, Page.stars)
-
         if searcher:
             if decimal_sep == '.':
                 stars[b] = float(searcher[0].get_text()[:3])
@@ -221,11 +184,6 @@ def get_stars(boxes_array, Page, country='mx', test_all=False, test_len=False, p
                 stars[b] = float(searcher[0].get_text()[:3].replace(decimal_sep,''))
 
         b +=1
-    if test_all == True:
-        print(stars)
-    
-    elif test_len == True:
-        print('stars:', len(stars))
     return stars
 
 def get_reviews(boxes_array, Page, country='mx', test_all=False, test_len=False, position=None):
@@ -236,7 +194,6 @@ def get_reviews(boxes_array, Page, country='mx', test_all=False, test_len=False,
     for box in boxes_array:
         #Remember that boxes are arrays
         searcher = search_boxes(box, Page.reviews)
-
         if searcher:
             if len(searcher) > 1:
                 searcher = [searcher[0]]
@@ -247,13 +204,9 @@ def get_reviews(boxes_array, Page, country='mx', test_all=False, test_len=False,
                 pass
         
         b +=1
-    if test_all == True:
-        print(reviews)
-    
-    elif test_len == True:
-        print('reviews:', len(reviews))
     return reviews
 
+# The name saids all
 def amazon_products_id(boxes_array, test=False):
     ids = [None]*len(boxes_array)
 
@@ -265,7 +218,5 @@ def amazon_products_id(boxes_array, test=False):
             ids[b] = 'www.amazon.com.mx/dp/' + product_id
             
         b +=1
-    if test == True:
-        print(ids)
-    return ids
 
+    return ids
